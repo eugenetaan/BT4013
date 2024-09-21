1fe465d747e96eaabf625401307dfe0aa4c8e57c
Maxim Yam <maximyam@Maxims-MacBook-Pro.local>
2024-09-21 18:52:18 +0800

    generation v1

--- generation.py
+++ generation.py
@@ -7,7 +7,11 @@ with open(file_path,'r') as config_file:
     os.environ["GROQ_API_KEY"] = config['api_key']
 
 
+
+import pandas as pd
 from llm import *
+from langchain_core.prompts import ChatPromptTemplate
+from pprint import pprint
 
 
 #Testing llm
@@ -18,11 +22,48 @@ print('Response: ',ans.content, '\t Model: ',ans.response_metadata['model_name']
 
 
 #Pipeline relevant data 
+#  Input --> list of text of trendy news articles 
+#  Output --> {Summarised trend: , severity of information, solutions}
 
-#   Input --> list of text of trendy news articles 
 
-#   Output --> {Summarised trend: , severity of information, solutions}
-import pandas as pd
-testset= []
+llama31_70bchain_risk = explainprompt_template | llama31_70b
+llama31_70bchain_severity = severityprompt_template | llama31_70b.with_structured_output(Severity)
+llama31_70bchain_analysis = explainprompt_template | llama31_70b
 
-pd.read_table('1.txt')
+
+
+
+def severityanalysis(texts):
+    output = llama31_70bchain_severity.invoke(texts)
+    return output.subcategory
+
+def explanationanalysis(texts):
+    output = llama31_70bchain_risk.invoke(texts)
+    return output.content
+
+
+def reactionanalysis(texts):
+    output = llama31_70bchain_analysis.invoke(texts)
+    return output.content
+
+
+#Main call function
+def call_analysis(texts): 
+    output1 = severityanalysis(texts) #return Severity of status
+    output2 = explanationanalysis(texts) #return Explanation of text articles
+    output3 = reactionanalysis(texts) #return How to deal with these articles 
+    return [output1, output2, output3]
+
+def main():
+    testset= []
+    files = ['1.txt','2.txt','3.txt']
+    for i in files:
+        with open(i, 'r') as file:
+            content1 = file.read()  # Reads the entire content of the file
+            testset.append(str(content1))
+    explanation = call_analysis(testset)
+    pprint(explanation)
+    return explanation 
+
+if __name__ == "__main__":
+    main()
